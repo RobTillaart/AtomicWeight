5c40394a8a7b69e1d388af4fe0eb2949a7c4daef
Rob Tillaart <rob.tillaart@gmail.com>
2023-04-15 19:41:58 +0200

    final cleanup

--- AtomicWeight.h
+++ AtomicWeight.h
@@ -40,12 +40,16 @@ public:
   uint8_t size();
 
 
+  //  BASIC
+  char *  name(const uint8_t el);
+  uint8_t find(const char * abbrev);
+
   uint8_t electrons(const uint8_t el);
   uint8_t neutrons(const uint8_t el);
   uint8_t protons(const uint8_t el);
 
 
-  //  weight of one atom
+  //  WEIGHT of one atom
   float   weight(const uint8_t el);
   //  if (el != NULL) weights one element in a formula, e.g el == "H"
   //  if (el == NULL) weights the whole formula
@@ -54,33 +58,26 @@ public:
   float   massPercentage(const char * formula, const char * abbrev);
 
 
-  char *  name(const uint8_t el);
-  uint8_t find(const char * abbrev);
-
-
   //  CONVERSION
   float   moles2grams(const char * formula, float moles = 1.0);
   float   grams2moles(const char * formula, float grams = 1.0);
 
 
-  //  DEBUG
-  float   weightFactor();
-
-
-  ////////////////////////////////////////////////////////////////
-  //
-  //  EXPERIMENTAL 0.1.4
-  //
   //  SPLIT FORMULA IN ELEMENTS
   uint8_t splitElements(const char * formula);
   uint8_t element(uint8_t el);
 
 
+  //  COUNT
   //  if (el != NULL) count atoms of one element in a formula, e.g el == "H"
   //  if (el == NULL) count all atoms in the whole formula
   uint32_t count(const char * formula, const char * el = NULL);
   //  atom percentage of one element in a formula.
   float   atomPercentage(const char * formula, const char * el);
+
+
+  //  DEBUG
+  float   weightFactor();
 
 
 private:
