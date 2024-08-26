ed68058ffc72df118540ea07f6925214859fc845
Rob Tillaart <rob.tillaart@gmail.com>
2024-08-26 17:30:53 +0200

    add minerals.h

---
+++ minerals.h
@@ -0,0 +1,26 @@
+#pragma once
+//
+//    FILE: minerals.h
+//  AUTHOR: Rob Tillaart
+//    DATE: 2024-08-12
+// PURPOSE: very short list of chemical formulae of minerals
+//     URL: https://github.com/RobTillaart/AtomicWeight
+
+
+/////////////////////////////////////////////////////////////////////////
+//
+//  minerals 
+//  to be extended 
+//
+
+
+#define FAYALITE            "Fe2SiO4"
+#define FORSTERITE          "Mg2SiO4" 
+#define MUSCOVITE           "KAl3Si3O10(OH)2"
+#define PYRITE              "FeS2"
+#define TREMOLITE           "Ca2Mg5Si8O22(OH)2"
+#define ZIRCON              "ZrSiO4" 
+
+
+//  -- END OF FILE --
+
